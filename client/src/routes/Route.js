84325be5b51540d7687c42d91851a27bddb940b1
Ivan Augusto Hoinacki <ivan@limbersoftware.com.br>
2020-02-07 22:47:35 -0300

    configs root import devdependences

--- client/src/routes/Route.js
+++ client/src/routes/Route.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Redirect, Route } from 'react-router-dom';
 
-import AuthLayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
 
 export default function RouterWrapper({
     // aqui eu faco a verificar das nossas paginas privadas
