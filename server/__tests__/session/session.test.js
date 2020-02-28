834d3beea0e3a2a8d9f2516b242c6d75966f89ee
Ivan A. Hoinacki <ivanhweb@gmail.com>
2020-02-28 16:06:35 -0300

    test session autenticate

--- server/__tests__/session/session.test.js
+++ server/__tests__/session/session.test.js
@@ -3,6 +3,11 @@ import jwt from 'jsonwebtoken';
 import app from '../src/app';
 
 describe('Session', () => {
+  /**
+   * A criacao dos testes referece a ordem de codificacao do controller, cada condicao
+   * deve ser testada.
+   * basedo no grafico informado no coverage
+   */
   it('Se eu enviar um usuario e senha valido ele retorna um token jwt', async done => {
     const response = await request(app)
       .post('/sessions')
@@ -12,8 +17,8 @@ describe('Session', () => {
       });
 
     let decoded = jwt.decode(response.body.token, { complete: true });
-    expect(response.body.token).toBeDefined();
-    expect(decoded.payload.email).toBe('ivanhweb@gmail.com');
+    expect(response.body.token).toBeDefined(); // token retornado e decodificado
+    expect(decoded.payload.email).toBe('ivanhweb@gmail.com'); // email enviado decodificado
     done();
   });
 });
